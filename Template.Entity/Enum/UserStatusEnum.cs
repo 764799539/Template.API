758d5bc803b1b9bd897613d9aca49641caa9654a
刘沛琦 <764799539@qq.com>
2019-12-02 11:36:01 +0800

    SnowFlakeID算法

---
+++ Template.Entity/Enum/UserStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Template.Model
+{
+    /// <summary>
+    /// 用户状态枚举类
+    /// </summary>
+    public enum UserStatusEnum
+    {
+        [Description("删除")]
+        Delete = -99,
+        
+    }
+}
--- Template/Startup.cs
+++ Template/Startup.cs
@@ -22,9 +22,13 @@ namespace Template
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        /// <summary>
+        /// 此方法由运行时调用，此方法将服务注入到容器，注入体现的是一个IOC（控制反转的的思想）
+        /// </summary>
+        /// <param name="services">依赖注入容器</param>
+        public void ConfigureServices(IServiceCollection services) 
         {
+            // 如services.AddTransient<IFoo, Foo>();向容器中注入接口和实现[依赖注入(DI)]
             services.AddControllers()
                 .AddNewtonsoftJson();
         }
